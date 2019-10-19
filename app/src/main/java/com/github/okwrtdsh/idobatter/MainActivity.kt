ea17af5cd5d11dc2e19f151b9f2d82a9daa42fb0
okwrtdsh <okwrtdsh@gmail.com>
2019-10-19 10:20:47 +0900

    first commit

---
+++ app/src/main/java/com/github/okwrtdsh/idobatter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.okwrtdsh.idobatter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
